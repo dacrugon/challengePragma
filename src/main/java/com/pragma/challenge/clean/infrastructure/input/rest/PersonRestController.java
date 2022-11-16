7dff6300a1a34c9be3e31331f686f29f5b92e23a
dacrugon <dario.gonzalez@pragma.com.co>
2022-11-16 11:47:35 -0500

    swagger

--- src/main/java/com/pragma/challenge/clean/infrastructure/input/rest/PersonRestController.java
+++ src/main/java/com/pragma/challenge/clean/infrastructure/input/rest/PersonRestController.java
@@ -3,6 +3,10 @@ package com.pragma.challenge.clean.infrastructure.input.rest;
 import com.pragma.challenge.clean.application.dto.PersonRequest;
 import com.pragma.challenge.clean.application.handler.IPersonHandler;
 import com.pragma.challenge.clean.common.response.ResponseHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,28 +18,39 @@ import org.springframework.web.bind.annotation.*;
 public class PersonRestController {
     private final IPersonHandler personHandler;
 
+    @Operation(summary = "Save a person")
     @PostMapping("/people")
     public ResponseEntity<Object> savePerson(PersonRequest personRequest){
         personHandler.savePerson(personRequest);
         return ResponseHandler.generateResponseWithoutData("P-201","successful", HttpStatus.OK);
     }
+
+    @Operation(summary = "Get all persons")
     @GetMapping("/people")
     public ResponseEntity<Object> getAllPersons(){
         return ResponseHandler.generateResponseWithData("P-201","successful",personHandler.getAllPersons(),HttpStatus.OK);
     }
+
+    @Operation(summary = "Get a person by identification number")
     @GetMapping("/people/{in}")
     public ResponseEntity<Object> getPersonByIdentificationNumber(@PathVariable String in){
         return ResponseHandler.generateResponseWithData("P-201","successful",personHandler.getPersonByIdentificationNumber(in),HttpStatus.OK);
     }
+
+    @Operation(summary = "Get all persons with age greater than or equal to the specified age")
     @GetMapping("/people/greaterThanOrEqualTo/{age}")
     public ResponseEntity<Object> getPeopleByAgeGreaterThanOrEqualsTo(@PathVariable Integer age){
         return ResponseHandler.generateResponseWithData("P-201","successful",personHandler.findPeopleByAgeGreaterThanOrEqualsTo(age),HttpStatus.OK);
     }
+
+    @Operation(summary = "Update a person by identification number")
     @PutMapping("/people/{in}")
     public ResponseEntity<Object> updatePerson(PersonRequest personRequest, @PathVariable String in){
         personHandler.updatePerson(personRequest,in);
         return ResponseHandler.generateResponseWithoutData("P-202","Updated",HttpStatus.OK);
     }
+
+    @Operation(summary = "Delete a person by identification number")
     @DeleteMapping("people/{in}")
     public ResponseEntity<Object> deletePersonByIdentificationNumber(@PathVariable String in){
         personHandler.deletePersonByIdentificationNumber(in);
--- src/main/java/com/pragma/challenge/clean/infrastructure/input/rest/PictureRestController.java
+++ src/main/java/com/pragma/challenge/clean/infrastructure/input/rest/PictureRestController.java
@@ -3,6 +3,7 @@ package com.pragma.challenge.clean.infrastructure.input.rest;
 import com.pragma.challenge.clean.application.dto.PictureRequest;
 import com.pragma.challenge.clean.application.handler.IPictureHandler;
 import com.pragma.challenge.clean.common.response.ResponseHandler;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -26,22 +27,26 @@ public class PictureRestController {
     @Value("${project.image}")
     private String path;
 
+    @Operation(summary = "Save a picture")
     @PostMapping("/pictures")
     public ResponseEntity<Object> savePicture(PictureRequest pictureRequest){
         pictureHandler.savePicture(pictureRequest);
         return ResponseHandler.generateResponseWithoutData("P-201","successful", HttpStatus.OK);
     }
 
+    @Operation(summary = "Get all pictures")
     @GetMapping("/pictures")
     public ResponseEntity<Object> getPictures(){
        return ResponseHandler.generateResponseWithData("P-201","successful",pictureHandler.getAllPictures(),HttpStatus.OK);
     }
 
+    @Operation(summary = "Get a picture by identification number")
     @GetMapping("/pictures/{in}")
     public ResponseEntity<Object> getPictureByIdentificationNumber(@PathVariable String in){
         return ResponseHandler.generateResponseWithData("P-201","successful",pictureHandler.getPictureByIdentificationNumber(in),HttpStatus.OK);
     }
 
+    @Operation(summary = "Download a picture by identification number")
     @GetMapping(value="/pictures/download/{in}",produces = MediaType.IMAGE_JPEG_VALUE)
     public void downloadPictureByIdentificationNumber(@PathVariable String in, HttpServletResponse response){
         InputStream resource = pictureHandler.getImageResource(in);
@@ -53,12 +58,14 @@ public class PictureRestController {
         }
     }
 
+    @Operation(summary = "Update a picture by identification number")
     @PutMapping("/pictures/{in}")
     public ResponseEntity<Object> updatePictureByIdentificationNumber(@PathVariable String in, MultipartFile imageFile){
         pictureHandler.updatePictureByIdentificationNumber(in,imageFile);
         return ResponseHandler.generateResponseWithoutData("P-202","Updated",HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete a picture by identification number")
     @DeleteMapping("/pictures/{in}")
     public ResponseEntity<Object> deletePictureByIdentificationNumber(@PathVariable String in){
         pictureHandler.deletePictureByIdentificationNumber(in);
